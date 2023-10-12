9db22f315bd59354d2a3b29c8b806347938b1268
DenisNemirich <denTimer@yandex.ru>
2023-10-12 11:54:20 +0200

    Сделал task4

---
+++ task4/Program.cs
@@ -0,0 +1,24 @@
+﻿// На вход поступает три числа и выдаётся максимальное из этих чисел.
+
+using System;
+
+System.Console.WriteLine("Введите первое число");
+    int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите второе число");
+    int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите третье число");
+    int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+    int max = firstNumber;
+        if (max < secondNumber)
+        {
+            max = secondNumber;
+        }
+        if (max < thirdNumber)
+        {
+            max = thirdNumber;
+        }
+            
+        System.Console.WriteLine("Максимальное число = " + max);
