f70f83a0d18f86b3e2776f7b5c9bcb6f2e72c600
DenisNemirich <denTimer@yandex.ru>
2023-10-13 15:44:32 +0200

    Сделал task8

---
+++ task8/Program.cs
@@ -0,0 +1,13 @@
+﻿// На вход поступает число (number), а на выходе выводит все чётные числа от 1 до number (включительно), разделеные знаком табуляции.
+
+using System;
+
+int number = new Random().Next(1, 10);
+System.Console.WriteLine("Выпало число: " + number);
+
+        for (int i = 1; i < number + 1; i++)
+    {
+        if(i % 2 == 0)
+            Console.Write(i +"\t");
+    }
+    
