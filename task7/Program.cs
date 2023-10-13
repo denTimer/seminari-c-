9f667b47a6a9e670168712f4c9621b31df81a6cb
DenisNemirich <denTimer@yandex.ru>
2023-10-13 15:38:04 +0200

    Сделал task7

---
+++ task7/Program.cs
@@ -0,0 +1,15 @@
+﻿/* 7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+456 -> 6
+782 -> 2
+918 -> 8
+*/
+
+Console.Clear();
+
+int number = new Random().Next(100, 1000);
+System.Console.WriteLine("Выпало число: " + number);
+
+int last = number % 10;
+
+System.Console.WriteLine("Последняя цифра - " + last);
