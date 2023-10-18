7451015d12b660603b580168b2981bc626207782
DenisNemirich <denTimer@yandex.ru>
2023-10-18 13:49:48 +0200

    Удалил задачи с лекций

--- lek_1/3_Sum/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿int numberA = new Random().Next(1,10);
-int numberB = new Random().Next(1,10);
-System.Console.WriteLine("Первое число: " + numberA);
-System.Console.WriteLine("Второе число: " + numberB);
-
-int result = numberA + numberB;
-Console.WriteLine("Сумма чисел равна: " + result);
--- lek_1/4_Delenie/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿double numberA = 12;
-double numberB = 5;
-//int result = numberA + numberB;
-Console.WriteLine(numberA / numberB);
