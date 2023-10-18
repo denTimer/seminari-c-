637333b4a84929337c5df0b3f788bc714b9d7d33
DenisNemirich <denTimer@yandex.ru>
2023-10-18 11:25:02 +0200

    Добавил random в lek_1_Sum

--- lek_1/3_Sum/Program.cs
+++ lek_1/3_Sum/Program.cs
@@ -1,4 +1,7 @@
-﻿int numberA = 3345;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
+System.Console.WriteLine("Первое число: " + numberA);
+System.Console.WriteLine("Второе число: " + numberB);
+
 int result = numberA + numberB;
-Console.WriteLine(result);
+Console.WriteLine("Сумма чисел равна: " + result);
