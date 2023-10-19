79d451e5a02d2217cc5e75a673e29d052d6f891f
«denTimer» <dentimer@yandex.ru>
2023-10-19 14:09:23 +0200

    Исправил task7

--- task7/Program.cs
+++ task7/Program.cs
@@ -8,8 +8,8 @@
 Console.Clear();
 
 int number = new Random().Next(100, 1000);
-System.Console.WriteLine("Выпало число: " + number);
+System.Console.WriteLine($"Выпало число: {number}");
 
 int last = number % 10;
 
-System.Console.WriteLine("Последняя цифра - " + last);
+System.Console.WriteLine($"Последняя цифра - {last}");
