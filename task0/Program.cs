924387d6161317640b3813e441130401a4480d63
«denTimer» <dentimer@yandex.ru>
2023-10-19 12:22:29 +0200

    Исправил task0

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,5 +1,5 @@
 ﻿// 0. Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
-System.Console.Write("Задай число:");
+Console.Write("Задай число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int kvadrat = number * number;
-System.Console.WriteLine(number + " в квадрате равно " + kvadrat);
+Console.WriteLine(number + " в квадрате равно " + kvadrat);
