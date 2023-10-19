d7dc7bdd5419dec97689f3e27d9d123c5b0b46fb
«denTimer» <dentimer@yandex.ru>
2023-10-19 13:17:26 +0200

    Исправил task5

--- task5/Program.cs
+++ task5/Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 // Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
-        Console.Write("Введите число N: ");
-        int N = Convert.ToInt32(Console.ReadLine());
-        
-        // Выводим все целые числа от -N до N
-        for (int i = -N; i <= N; i++)
-        {
-            Console.Write(i +"\t");
-        }
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+// Выводим все целые числа от -N до N
+for (int i = -N; i <= N; i++)
+{
+    Console.Write(i + "\t");
+}
 
