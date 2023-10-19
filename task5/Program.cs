88b24be8f2953a2ec201fab0fb4b1a1d5fa9169a
«denTimer» <dentimer@yandex.ru>
2023-10-19 13:13:20 +0200

    Исправил task5

--- task5/Program.cs
+++ task5/Program.cs
@@ -1,18 +1,11 @@
-﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
-
-using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
+﻿using System;
+// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
         Console.Write("Введите число N: ");
-        int N = int.Parse(Console.ReadLine());
+        int N = Convert.ToInt32(Console.ReadLine());
         
         // Выводим все целые числа от -N до N
         for (int i = -N; i <= N; i++)
         {
             Console.Write(i +"\t");
         }
-    }
-}
+
