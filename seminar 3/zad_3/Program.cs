4fa694f45a68ed5337100edfb2bc33427b3ae09f
«denTimer» <dentimer@yandex.ru>
2023-11-25 16:49:53 +0200

    Сделал 3 задания 3 семинара

---
+++ seminar 3/zad_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте массив из вещественных чисел с ненулевой дробной частью. Найдите разницу между максимальным и минимальным элементов массива.
+
+
+double[] numbers = new double[] { 3.5, 8.7, 12.9, 17.2, 20.6, 25.1, 30.4, 35.8, 40.3, 45.0 };
+            double min = numbers[0];
+            double max = numbers[0];
+
+            foreach (var number in numbers)
+            {
+                if (number < min)
+                {
+                    min = number;
+                }
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+
+            double difference = max - min;
+            Console.WriteLine("Разница между максимальным и минимальным элементами массива: " + difference);
---
+++ seminar 3/zad_4/Program.cs
@@ -0,0 +1,15 @@
+﻿int number = 245631; // Здесь можно указать любое натуральное число от 1 до 100000
+            int numLength = (int)Math.Floor(Math.Log10(number) + 1); // Определение количества цифр в числе
+            int[] digitsArray = new int[numLength]; // Создание массива
+
+            for (int i = numLength - 1; i >= 0; i--)
+            {
+                digitsArray[i] = number % 10; // Заполнение массива цифрами числа
+                number /= 10; // Удаление последней цифры числа
+            }
+
+            Console.WriteLine("Массив цифр числа:");
+            foreach (var digit in digitsArray)
+            {
+                Console.Write(digit + " ");
+            }
